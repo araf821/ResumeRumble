9fdc5433d15014fc6679ecc0274e073c6d5c01cb
Gigi <gigi_cheang@hotmail.com>
2023-10-14 18:03:51 -0400

    Fix to rating page

--- components/AddFileCard.tsx
+++ components/AddFileCard.tsx
@@ -7,15 +7,15 @@ import { FC } from "react";
 interface AddFileCardProps {}
 
 const AddFileCard: FC<AddFileCardProps> = ({}) => {
-  const {onOpen} = useModal();
+  const { onOpen } = useModal();
 
   return (
     <button
-      className="lg:text-lg border-dashed border-zinc-600 border-2 flex w-full max-w-md rounded-md flex-col items-center justify-center h-full"
+      className="flex h-[30dvh] w-full flex-col items-center justify-center rounded-md border-2 border-dashed border-zinc-600 lg:text-lg"
       onClick={() => onOpen("addFile")}
     >
       <p className="text-zinc-400">Add New File</p>
-      <PlusCircle className="text-zinc-400 w-5 h-5" />
+      <PlusCircle className="h-5 w-5 text-zinc-400" />
     </button>
   );
 };
