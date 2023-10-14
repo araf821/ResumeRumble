a95041850d4f7ec9273f4ccdbc4444131da81332
araf821 <araf.ahmed200@gmail.com>
2023-10-14 01:47:12 -0400

    initialized prisma and uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,20 @@
+import { auth } from "@clerk/nextjs";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  return { userId };
+};
+
+export const ourFileRouter = {
+  pdf: f({ pdf: { maxFileCount: 1, maxFileSize: "8MB" } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
