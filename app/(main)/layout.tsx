d68402967ff8fd484684f97c759eb435890abc9e
Gigi <gigi_cheang@hotmail.com>
2023-10-14 13:31:00 -0400

    UI and code cleanup

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,8 @@
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-[90dvh] w-screen bg-zinc-900">{children}</div>;
+  return (
+    <div className="h-[90dvh] w-screen overflow-clip bg-zinc-900">
+      {children}
+    </div>
+  );
 };
 export default layout;
--- app/(main)/rating/page.tsx
+++ app/(main)/rating/page.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
-
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import pdfIcon from "@/assets/pdf.svg";
 
 const Rating = () => {
   return (
-	<div>
-		<div className="mx-8 my-5 rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 px-5 py-5">
-        <h1 className=" overflow-clip whitespace-nowrap text-xl font-bold">
-          Resume rankings
-        </h1>
-        <h2 className="text-lg">See how you rank!</h2>
+    <>
+      <div className="page-body grid grid-cols-2 gap-5">
+        <div className="input-box w-full">
+          <h2 className="text-neutral-200">job posting:</h2>
+          <Textarea
+            className="rounded-xl border border-neutral-200 bg-zinc-800 text-neutral-200"
+            placeholder="<Paste job description here. If you’re interested in general feedback, leave this empty.>"
+          />
+        </div>
+        <div className="resume-sec w-full">
+          <h2 className="text-neutral-200">your resume:</h2>
+          <div className="grid w-full">
+            <div className="mx-auto my-10">
+              <Image src={pdfIcon} layout="fixed" alt="pdf" />
+            </div>
+            <Input
+              type="file"
+              className="w-full rounded-xl border border-neutral-200 bg-zinc-800 text-sky-500"
+            />
+          </div>
+          <Button className="mt-5 w-full bg-sky-500">RATE IT BABY!!!</Button>
+          <div className="submit button"></div>
+        </div>
       </div>
-		<div className="page-body flex flex-row wscreen justify-around">
-			<div className="input box">
-				<h2>job posting:</h2>
-				<Textarea className='w-max' placeholder="<Paste job description here. If you’re interested in general feedback, leave this empty.>" />
-			</div>
-			<div className="resume sec">
-				<h2>your resume:</h2>
-				<div className="icon mt-3"></div>
-					<div className=" mt-3 grid w-full max-w-sm items-center gap-1.5">
-						<Input id="mt-3 picture" type="file" />
-					</div>
-				<Button>Submit</Button>
-				<div className="submit button"></div>
-			</div>
-		</div>
-	</div>
-  )
-}
+    </>
+  );
+};
 
-export default Rating
+export default Rating;
