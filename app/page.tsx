b3ae748533b744f5d71a9efd11e207f60f01178f
emmimelon <emmimelon@gmail.com>
2023-10-14 21:11:02 -0400

    some animations, and re-arranged header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { SignInButton } from "@clerk/nextjs";
 import { Box } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import {motion} from 'framer-motion'
 
 export default function Home() {
   const router = useRouter()
@@ -14,42 +16,85 @@ export default function Home() {
   return (
 
     <div className="w-full min-h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
-      <h1 className="pt-10 indent-12 text-xl text-cyan-100">
-        Welcome To
-      </h1>
-      <h2 className="font-bold pt-5 indent-24 text-left text-8xl text-sky-500">
-        Resume Rumble
-      </h2>
-      <h3 className="indent-24 pr-1 text-left text-xl text-amber-500">
-        Level up
-        <span className="pl-4 indent-2 text-cyan-100">
-          your resume game!
-        </span>
-      </h3>
+      <motion.div initial={{opacity:0}} animate={{opacity:1}} className = " py-5">
+        <h1 className="mt-40 text-center text-xl text-cyan-100">
+          Welcome To
+        </h1>
+        <div className="text-center font-bold text-9xl text-sky-500">
+          <motion.span className="inline-block" variants={{
+            hidden: {
+              opacity: 0,
+              x: -1000,
+              
+            },
+            show: {
+              opacity: 1,
+              x: 0,
+              
+            },
+          }}
+          animate="show"
+          initial="hidden" >
+            Resume 
+          </motion.span>   
+          <motion.span className="inline-block" variants={{
+            hidden: {
+              opacity: 0,
+              x: 1000,
+              
+            },
+            show: {
+              opacity: 1,
+              x: 0,
+              
+            },
+          }}
+          animate="show"
+          initial="hidden" >
+            Rumble 
+          </motion.span>   
+        </div>
+        
+        <h3 className="pb-5 text-center text-xl text-amber-500">
+          Level up
+          <span className="pl-4 indent-2 text-cyan-100">
+            your resume game!
+          </span>
+        </h3>
+      </motion.div>
+      
 
-      <div className="px-12 py-10">
-        <div className="rounded-md border-solid border-2 border-neutral-200 bg-opacity-25 bg-zinc-800 grid grid-cols-2">
-          <p className="px-20 py-40 grid-rows-8 col-start-1 text-left text-xl text-cyan-100">
+      <motion.div initial={{opacity:0}} animate={{opacity:1}} className="px-12 py-10 ">
+        <div className="my-20 rounded-md border-solid border-2 border-neutral-200 bg-opacity-25 bg-zinc-800">
+          <p className="w-1/2 mx-auto px-20 py-20 text-center text-xl text-cyan-100">
             Made by job seekers, for job seekers,
-            <span className="px-3 row-start-3 row-end-4 text-sky-500">
-              Resume Rumble
+            <span className="px-3 text-sky-500">
+              ResumeRumble
             </span>
             provides a free and fun way to
             improve your resume with detailed, competitive insights,
             powered by OpenAI’s generative models.
           </p>
-          <div className="grid grid-rows-4 col-start-2 py-30 px-40">
-            <Button className="text-xl mt-4 py-8 row-start-2 bg-sky-400" onClick={() => gotoLogin()}>
-              Login!
-            </Button>
-            <Button className="text-xl py-8 row-start-3 bg-sky-400" onClick={() => gotoLogin()}>
-              Sign-Up?
-            </Button>
+          <div className="space-x-10 grid grid-cols-2 py-30 px-40 mb-20">
+            <motion.div initial={{scale:0}} whileInView={{scale: [1, 1.05, 1.05, 1, 1], rotate: [0, 7, -4, 1, 0], }} viewport={{ once: true }}>
+              <Button className="text-xl mt-4 px-52 py-8 col-start-1 bg-sky-400">
+                <SignInButton>
+                Login! 
+                </SignInButton>
+              </Button> 
+            </motion.div> 
+            <motion.div initial={{scale:0}} whileInView={{scale: [1, 1.05, 1.05, 1, 1], rotate: [0, -8, 4, -2, 0], }} viewport={{ once: true }}>
+              <Button className="text-xl mt-4 px-52 py-8 col-start-2 bg-sky-400">
+                <SignInButton>
+                Sign‑up?
+                </SignInButton>
+              </Button>
+            </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="bg-zinc-800">
+      <motion.div initial={{opacity:0}} whileInView={{opacity:1}} viewport={{ once: true }} className="bg-zinc-800">
         <h1 className="pt-10 underline decoration-amber-500 underline-offset-4 decoration-2 indent-12 text-5xl text-cyan-100">
           Features
         </h1>
@@ -59,7 +104,7 @@ export default function Home() {
             <p className="indent-0 px-14 font-light mt-10 bg-zinc-900 bg-opacity-50 text-xl pt-20 pb-20 text-cyan-100">
               Get your resume rated out of 100!
               <p className="my-20">
-                Resume Rumble uses OpenAI to score the quality of your resume.
+                ResumeRumble uses OpenAI to score the quality of your resume.
                 Scoring can be done on the general quality of your resume, or 
                 specific to the job you're aiming for.
               </p>
@@ -72,14 +117,14 @@ export default function Home() {
                 Battle your resume against other users!
               <p className="text-left my-20">
                 Join a lobby, invite your friends, or connect with other users.
-                Resume Rumble will compare your resume against other users and see 
+                ResumeRumble will compare your resume against other users and see 
                 who comes out on top.
               </p>
-                Where do you stand on the leaderboard, sign up!
+                Find your place on the leaderboard, sign up!
             </p>
           </h2>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
