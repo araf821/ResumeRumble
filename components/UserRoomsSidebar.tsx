92a2ecc86290f804a8e358bec328b2b3d52dd591
araf821 <araf.ahmed200@gmail.com>
2023-10-15 09:55:43 -0400

    implemented user rooms sidebar functionality

--- components/UserRoomsSidebar.tsx
+++ components/UserRoomsSidebar.tsx
@@ -4,6 +4,8 @@ import { Room } from "@prisma/client";
 import { FC } from "react";
 import { Button } from "./ui/button";
 import { useModal } from "@/hooks/useModal";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 interface UserRoomsSidebarProps {
   rooms: Room[] | undefined;
@@ -29,11 +31,26 @@ const UserRoomsSidebar: FC<UserRoomsSidebarProps> = ({ rooms }) => {
           </Button>
         </div>
       ) : (
-        rooms.map((room) => <div key={room.id}>
-          
-        </div>)
+        <div className="space-y-4 p-4">
+          {rooms.map((room) => (
+            <div
+              key={room.id}
+              className="flex w-full items-center justify-between rounded-md border border-zinc-900 bg-zinc-700 px-3 py-2.5"
+            >
+              <p>{room.name}</p>
+              <Link href={`/ranking/room/${room.id}`}>
+                <ArrowRight />
+              </Link>
+            </div>
+          ))}
+          <Button
+            onClick={() => onOpen("createRoom")}
+            className="w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-lg font-bold text-black hover:bg-current"
+          >
+            Create Room
+          </Button>
+        </div>
       )}
-      
     </div>
   );
 };
