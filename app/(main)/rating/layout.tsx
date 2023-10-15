ad96812e2682723a8397d8dda50cf3043bd98670
Gigi <gigi_cheang@hotmail.com>
2023-10-14 22:22:20 -0400

    Swap gradients for contrast

--- app/(main)/rating/layout.tsx
+++ app/(main)/rating/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="px-8 py-5">
-        <div className="mb-8 rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-5">
+        <div className="mb-8 rounded-xl bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 p-5">
           <h1 className="overflow-clip whitespace-nowrap text-xl font-bold">
             Resume rating
           </h1>
