85b21e1c3106f028f2e9f82fd3ec6e0378b7eaa3
Gigi <gigi_cheang@hotmail.com>
2023-10-14 23:30:01 -0400

    Style updates for LobbyItem

--- components/LobbyItem.tsx
+++ components/LobbyItem.tsx
@@ -9,11 +9,16 @@ const LobbyItem = ({
   url?: string;
 }) => {
   return (
+    // show user count/limit
+    // join modal shows description and resume select
     <div className="my-5">
       <div className="flex overflow-clip rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5">
-        <p className="my-auto font-bold text-neutral-200">{name}</p>
+        <p className="my-auto overflow-clip overflow-ellipsis whitespace-nowrap font-bold text-neutral-200">
+          {name}
+        </p>
 
-        <Link className="my-auto ml-auto" href={url}>
+        <Link className="my-auto ml-auto flex" href={url}>
+          <p className="mr-5 hidden text-neutral-200 md:block">Join</p>
           <LogIn className="text-neutral-200" />
         </Link>
       </div>
