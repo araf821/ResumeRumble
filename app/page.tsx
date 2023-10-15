421a1ac638c637d559794a620f76b3158f16946c
Gigi <106707569+gcheang@users.noreply.github.com>
2023-10-14 21:58:52 -0400

    Landing page (#14)
    * Move ranking folder
    
    * Fix signin layout
    
    * Progress on landing page
    
    * Update dashboard style
    
    * Fix dashboard style
    
    * UI and code cleanup
    
    * UI overhaul
    
    * added features section to landing
    
    * Minor UI change
    
    * Minor style adjustments to dash
    
    * Update rating page
    Fix style issues
    Add select
    Now responsive
    
    * Change ranking page style
    
    * Update rating page
    
    * Small style updates
    Add dashboard link to rating
    
    * Small padding edit
    
    * Add plus to ranking page
    
    * Fix to rating page
    
    * Scroll fix
    
    * Add default resume button
    Code cleanup
    
    * Small style fix
    
    * Add ResumeFeedback component
    
    * some animations, and re-arranged header
    
    * Package lock
    
    ---------
    
    Co-authored-by: emmimelon <emmimelon@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   const router = useRouter();
@@ -10,73 +18,167 @@ export default function Home() {
   };
   return (
     <div className="min-h-screen w-full bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
-      <h1 className="pt-10 indent-12 text-xl text-cyan-100">Welcome To</h1>
-      <h2 className="pt-5 text-left indent-24 text-8xl font-bold text-sky-500">
-        Resume Rumble
-      </h2>
-      <h3 className="pr-1 text-left indent-24 text-xl text-amber-500">
-        Level up
-        <span className="pl-4 indent-2 text-cyan-100">your resume game!</span>
-      </h3>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className=" py-5"
+      >
+        <h1 className="mt-28 text-center text-xl text-cyan-100">Welcome To</h1>
+        <div className="text-center text-6xl font-bold text-sky-500 sm:text-7xl md:text-8xl">
+          <motion.span
+            className="inline-block"
+            variants={{
+              hidden: {
+                opacity: 0,
+                x: -1000,
+              },
+              show: {
+                opacity: 1,
+                x: 0,
+              },
+            }}
+            animate="show"
+            initial="hidden"
+          >
+            Resume
+          </motion.span>
+          <motion.span
+            className="inline-block"
+            variants={{
+              hidden: {
+                opacity: 0,
+                x: 1000,
+              },
+              show: {
+                opacity: 1,
+                x: 0,
+              },
+            }}
+            animate="show"
+            initial="hidden"
+          >
+            Rumble
+          </motion.span>
+        </div>
 
-      <div className="px-12 py-10">
-        <div className="grid grid-cols-2 rounded-md border-2 border-solid border-neutral-200 bg-zinc-800 bg-opacity-25">
-          <p className="grid-rows-8 col-start-1 px-20 py-40 text-left text-xl text-cyan-100">
+        <h3 className="pb-5 text-center text-lg text-amber-500 md:text-xl">
+          Level up
+          <span className="pl-4 indent-2 text-cyan-100">your resume game!</span>
+        </h3>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="mx-5 md:mx-12 "
+      >
+        <div className="mx-10 grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-7">
+          <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{
+              scale: [1, 1.05, 1.05, 1, 1],
+              rotate: [0, 7, -4, 1, 0],
+            }}
+            viewport={{ once: true }}
+          >
+            <Button className="w-full bg-sky-400 py-8 text-xl">
+              <SignInButton>Login!</SignInButton>
+            </Button>
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{
+              scale: [1, 1.05, 1.05, 1, 1],
+              rotate: [0, -8, 4, -2, 0],
+            }}
+            viewport={{ once: true }}
+          >
+            <Button className="w-full bg-sky-400 py-8 text-xl">
+              <SignInButton>Sign‑up?</SignInButton>
+            </Button>
+          </motion.div>
+        </div>
+        <div className="my-10 rounded-md border-2 border-solid border-neutral-200 bg-zinc-800 bg-opacity-25">
+          <p className="mx-auto w-5/6 whitespace-normal py-8 text-center text-xl text-cyan-100">
             Made by job seekers, for job seekers,
-            <span className="row-start-3 row-end-4 px-3 text-sky-500">
+            <span className="block whitespace-nowrap px-3 text-sky-500">
               Resume Rumble
             </span>
             provides a free and fun way to improve your resume with detailed,
             competitive insights, powered by OpenAI’s generative models.
           </p>
-          <div className="py-30 col-start-2 grid grid-rows-4 px-40">
-            <Button
-              className="row-start-2 mt-4 bg-sky-400 py-8 text-xl"
-              onClick={() => gotoLogin()}
-            >
-              Login!
-            </Button>
-            <Button
-              className="row-start-3 bg-sky-400 py-8 text-xl"
-              onClick={() => gotoLogin()}
-            >
-              Sign-Up?
-            </Button>
-          </div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="bg-zinc-800">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        className="bg-zinc-800"
+      >
         <h1 className="pt-10 indent-12 text-5xl text-cyan-100 underline decoration-amber-500 decoration-2 underline-offset-4">
           Features
         </h1>
-        <div className="mx-12 mt-10 grid grid-cols-2 space-x-4">
-          <h2 className="rounded-md bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 pt-10 text-left indent-12 text-3xl font-extrabold">
-            Resume Ratings
-            <p className="mt-10 bg-zinc-900 bg-opacity-50 px-14 pb-20 pt-20 indent-0 text-xl font-light text-cyan-100">
-              Get your resume rated out of 100!
-              <p className="my-20">
-                Resume Rumble uses OpenAI to score the quality of your resume.
-                Scoring can be done on the general quality of your resume, or
-                specific to the job you&rsquo;re aiming for.
-              </p>
-              What score do you think you&rsquo;ll get? Sign up now!
-            </p>
-          </h2>
-          <h2 className="rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pt-10 text-left indent-12 text-3xl font-extrabold">
-            Resume Rankings
-            <p className="mt-10 bg-zinc-900 bg-opacity-50 px-12 pb-20 pt-20 text-left indent-0 text-xl font-light text-cyan-100">
-              Battle your resume against other users!
-              <p className="my-20 text-left">
-                Join a lobby, invite your friends, or connect with other users.
-                Resume Rumble will compare your resume against other users and
-                see who comes out on top.
-              </p>
-              Where do you stand on the leaderboard, sign up!
-            </p>
-          </h2>
+        <div className="mx-8 mt-10 grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-5">
+          <Accordion
+            type="single"
+            collapsible
+            className="mb-8 h-fit w-full rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-5"
+          >
+            <AccordionItem value="1">
+              <AccordionTrigger>
+                <div>
+                  <h1 className="overflow-clip whitespace-nowrap text-left text-xl font-bold">
+                    Resume rating
+                  </h1>
+                  <h2 className="text-left text-lg">Get your resume ranked!</h2>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="text-xl font-light text-zinc-800">
+                  Get your resume rated out of 100!
+                  <p className="my-20">
+                    ResumeRumble uses OpenAI to score the quality of your
+                    resume. Scoring can be done on the general quality of your
+                    resume, or specific to the job you're aiming for.
+                  </p>
+                  What score do you think you&rsquo;ll get? Sign up now!
+                </p>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion
+            type="single"
+            collapsible
+            className="mb-8 h-fit w-full rounded-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-5"
+          >
+            <AccordionItem value="2">
+              <AccordionTrigger>
+                <div>
+                  <h1 className="overflow-clip whitespace-nowrap text-left text-xl font-bold">
+                    Resume Rankings
+                  </h1>
+                  <h2 className="text-left text-lg">See how you rank!</h2>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="text-xl font-light text-zinc-800">
+                  Battle your resume against other users!
+                  <p className="my-20">
+                    Join a lobby, invite your friends, or connect with other
+                    users. ResumeRumble will compare your resume against other
+                    users and see who comes out on top.
+                  </p>
+                  Find your place on the leaderboard, sign up!
+                </p>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
-      </div>
+        <div className="grid h-20 w-full place-content-center justify-center bg-black">
+          <p className="text-neutral-200">Made for HTV8 ❤️</p>
+        </div>
+      </motion.div>
     </div>
   );
 }
