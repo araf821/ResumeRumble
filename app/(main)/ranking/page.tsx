0bdf01f9f85b770bc2d62b5e04211866c1ef008b
JUB_Yoush <Yoush1337@gmail.com>
2023-10-14 23:53:20 -0400

    technically nothing

--- app/(main)/ranking/page.tsx
+++ app/(main)/ranking/page.tsx
@@ -1,6 +1,22 @@
+ "use client"
+import { Textarea } from "@/components/ui/textarea"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 import LobbyItem from "@/components/LobbyItem";
 import Link from "next/link";
 import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+ 
 
 const lobbyData = [
   { name: "Lobby 1" },
@@ -20,20 +36,38 @@ const lobbyData = [
   { name: "Lobby 15" },
 ];
 
-const RankingPage = () => {
-  return (
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
+ 
+
+  
+   
+ 
+
+ 
+ const RankingPage = () => {
+  
+ return (
     <div className="h-[60dvh] overflow-y-scroll rounded-xl bg-zinc-800 p-5 pt-2">
       {lobbyData.map((data, index) => (
         <LobbyItem key={index} name={data.name}></LobbyItem>
       ))}
-      <Link
+         <div
         className="grid h-28 content-center justify-items-center rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5"
-        href="#"
+       
       >
-        <Plus className="text-neutral-200" />
-      </Link>
+       <form action="">
+
+       </form>
+      </div>
     </div>
   );
 };
+
 
 export default RankingPage;
