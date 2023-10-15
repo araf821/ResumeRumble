bc538594bf3da4d91bd4fd77bafada7a729981c0
Gigi <gigi_cheang@hotmail.com>
2023-10-14 23:14:35 -0400

    Add delete buttons on dashboard Other style changes on dashboard

--- components/ResumeCard.tsx
+++ components/ResumeCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Trash2 } from "lucide-react";
 
 function ResumeCard({
   name,
@@ -13,7 +14,7 @@ function ResumeCard({
   isDefault?: boolean;
 }) {
   return (
-    <div className="resume-card group flex h-40 flex-col overflow-clip rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5">
+    <div className="resume-card flex h-40 flex-col overflow-clip rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5">
       <h1 className="text-neutral-200">{name}</h1>
       <div className="upload-date text-neutral-200">{uploadDate}</div>
       <Link
@@ -24,15 +25,20 @@ function ResumeCard({
       >
         View PDF Version
       </Link>
-      {isDefault === true ? (
-        <h1 className="mt-2 text-sm font-bold text-blue-500">
-          This is your default resume
-        </h1>
-      ) : (
-        <Button className="invisible mt-2 bg-sky-500 group-hover:visible">
-          Set as default
+      <div className="grid grid-cols-[auto_min-content] gap-2">
+        {isDefault === true ? (
+          <Button className="mt-2 bg-sky-700" disabled>
+            Default resume
+          </Button>
+        ) : (
+          <Button className="mt-2 border bg-zinc-800 hover:border-0 hover:bg-sky-500">
+            Set as default
+          </Button>
+        )}
+        <Button className="mt-2 border bg-zinc-800 hover:border-0 hover:bg-red-500">
+          <Trash2 />
         </Button>
-      )}
+      </div>
     </div>
   );
 }
