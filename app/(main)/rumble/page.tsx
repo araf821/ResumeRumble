223b01f4ee7fb72d00cd743ea94f2a10164de6fd
Gigi <gigi_cheang@hotmail.com>
2023-10-14 20:59:33 -0400

    Add ResumeFeedback component

--- app/(main)/rumble/page.tsx
+++ app/(main)/rumble/page.tsx
@@ -1,66 +1,58 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { FileText } from "lucide-react";
-import FileUpload from "@/components/FileUpload";
+"use client";
 import PDFViewer from "@/components/PDFViewer";
+import ResumeFeedback from "@/components/ResumeFeedback";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-
-let submittedResume = true;
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Rating = () => {
-  if (submittedResume) {
-    return (
-      //	<div className='grid grid-cols-1 md:grid-cols-2 text-white'>
-      <div>
-        {/* <div className="mx-8 my-5 rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 px-5 py-5">
-          <h1 className=" overflow-clip whitespace-nowrap text-xl font-bold">
-            Results:
-          </h1>
-          <h2 className="text-lg">Are you cooking!?</h2>
-        </div> */}
-        <div className="m-4 grid grid-cols-1 gap-2 text-white md:grid-cols-2">
-          <PDFViewer url="amongus" />
-          <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Grammar: 5/5</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger>Action Verbs: 5/5</AccordionTrigger>
-              <AccordionContent>
-                Yes. It comes with default styles that matches the other
-                components&apos; aesthetic.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>Content: 5/5</AccordionTrigger>
-              <AccordionContent>
-                Yes. It&apos;s animated by default, but you can disable it if
-                you prefer.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-4">
-              <AccordionTrigger>
-                (Optional) Keyword Matching: 5/5
-              </AccordionTrigger>
-              <AccordionContent>
-                Yes. It&apos;s animated by default, but you can disable it if
-                you prefer.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </div>
+  const data = [
+    {
+      title: "Grammar: 5/5",
+      content: "Yes. It adheres to the WAI-ARIA design pattern.",
+    },
+    {
+      title: "Action Verbs: 5/5",
+      content:
+        "Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+    },
+    {
+      title: "Content: 5/5",
+      content:
+        "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+    },
+    {
+      title: "(Optional) Keyword Matching: 5/5",
+      content:
+        "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+    },
+    {
+      title: "(Optional) Keyword Matching: 5/5",
+      content:
+        "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+    },
+    {
+      title: "(Optional) Keyword Matching: 5/5",
+      content:
+        "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+    },
+  ];
+  return (
+    <>
+      <ResumeFeedback data={data} />
+      <ResumeFeedback />{" "}
+      <div className="mb-5 h-24 resize-none rounded-xl border border-neutral-200 bg-zinc-800 p-5 text-neutral-200">
+        The winner is...
       </div>
-    );
-  } 
+      <Link className="mt-5 w-full" href="/rating">
+        <Button className="w-full bg-sky-500">rate another resume</Button>
+      </Link>
+    </>
+  );
 };
-
 export default Rating;
