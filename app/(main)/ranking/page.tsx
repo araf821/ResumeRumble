24b9addb447c861fb92081bb16834ebdb2cc308c
araf821 <araf.ahmed200@gmail.com>
2023-10-15 06:37:39 -0400

    initialized room and ranking system

--- app/(main)/ranking/page.tsx
+++ app/(main)/ranking/page.tsx
@@ -1,29 +1,20 @@
- "use client"
-import { Textarea } from "@/components/ui/textarea"
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import * as z from "zod";
 import LobbyItem from "@/components/LobbyItem";
-import Link from "next/link";
-import { Plus } from "lucide-react";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 import {
   AlertDialog,
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-
-
-
- 
+} from "@/components/ui/alert-dialog";
 
 const lobbyData = [
   { name: "Lobby 1", current: 1, max: 2 },
@@ -43,61 +34,65 @@ const lobbyData = [
   { name: "Lobby 15" },
 ];
 
-
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-})
- 
- 
+});
 
-  
-   
- 
-
- 
- const RankingPage = () => {
-  
- return (
-    <div className="h-[60dvh] overflow-y-scroll rounded-xl bg-zinc-800 p-5 pt-2">
-      {lobbyData.map((data, index) => (
-        <LobbyItem
-          key={index}
-          name={data.name}
-          current={data.current}
-          max={data.max}
-        ></LobbyItem>
-      ))}
-         <div
-        className="grid h-40 content-center justify-items-center rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5"
-       
-      >
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button className="text-black" variant="outline">Make Lobby</Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent className="bg-zinc-800">
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Make Lobby:</AlertDialogTitle>
-                </AlertDialogHeader>
-                <Input type="name" placeholder="Lobby Name" />
-                <Textarea
-              className="h-32-xl border border-neutral-200 bg-zinc-800 text-neutral-200"
-              placeholder="<Paste job description here. If you’re interested in general feedback, leave this empty.>"
-            />
-                <AlertDialogFooter>
-                  <AlertDialogCancel className="text-black"  >Cancel</AlertDialogCancel>
-                  <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-        
-        
+const RankingPage = () => {
+  return (
+    <div className="mx-auto max-w-screen-xl px-4 py-8 2xl:px-0">
+      <div className="mb-8 rounded-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-5">
+        <p className="text-xl font-bold md:text-2xl">
+          Resume Ranking
+        </p>
+        <h2 className="text-lg">See how you rank against others!</h2>
       </div>
     </div>
+
+    // <div className="h-[60dvh] rounded-xl bg-zinc-800 p-5 pt-2">
+    //   <div className="mb-8 rounded-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-5">
+    //     <h1 className="overflow-clip whitespace-nowrap text-xl font-bold">
+    //       Resume ranking
+    //     </h1>
+    //     <h2 className="text-lg">See how you rank!</h2>
+    //   </div>
+    //   {lobbyData.map((data, index) => (
+    //     <LobbyItem
+    //       key={index}
+    //       name={data.name}
+    //       current={data.current}
+    //       max={data.max}
+    //     ></LobbyItem>
+    //   ))}
+    //   <div className="grid h-40 content-center justify-items-center rounded-xl border border-neutral-200 bg-zinc-800 px-5 py-5">
+    //     <AlertDialog>
+    //       <AlertDialogTrigger asChild>
+    //         <Button className="text-black" variant="outline">
+    //           Make Lobby
+    //         </Button>
+    //       </AlertDialogTrigger>
+    //       <AlertDialogContent className="bg-zinc-800">
+    //         <AlertDialogHeader>
+    //           <AlertDialogTitle>Make Lobby:</AlertDialogTitle>
+    //         </AlertDialogHeader>
+    //         <Input type="name" placeholder="Lobby Name" />
+    //         <Textarea
+    //           className="h-32-xl border border-neutral-200 bg-zinc-800 text-neutral-200"
+    //           placeholder="<Paste job description here. If you’re interested in general feedback, leave this empty.>"
+    //         />
+    //         <AlertDialogFooter>
+    //           <AlertDialogCancel className="text-black">
+    //             Cancel
+    //           </AlertDialogCancel>
+    //           <AlertDialogAction>Continue</AlertDialogAction>
+    //         </AlertDialogFooter>
+    //       </AlertDialogContent>
+    //     </AlertDialog>
+    //   </div>
+    // </div>
   );
 };
-
 
 export default RankingPage;
--- app/(main)/rating/page.tsx
+++ app/(main)/rating/page.tsx
@@ -18,7 +18,7 @@ const Rating = async () => {
     <div className="mx-auto max-w-screen-xl space-y-6 px-4 py-8 2xl:px-0">
       <div className="mb-8 rounded-xl bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 p-5">
         {" "}
-        <h1 className="overflow-clip whitespace-nowrap text-xl font-bold md:text-2xl">
+        <h1 className="text-xl font-bold md:text-2xl">
           Resume Rating
         </h1>
         <h2 className="text-lg">Get your resume ranked and reviewed!</h2>
@@ -27,103 +27,6 @@ const Rating = async () => {
       <RatingForm files={files} />
     </div>
   );
-  // var [submittedResume, setSubmittedResume] = useState(false);
-
-  // if (submittedResume) {
-  //   return (
-  //     <div className="mx-auto  max-w-screen-xl">
-  //       <div className="mb-8 rounded-xl bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 p-5">
-  //         <h1 className="overflow-clip whitespace-nowrap text-xl font-bold">
-  //           Resume rating
-  //         </h1>
-  //         <h2 className="text-lg">Get your resume ranked!</h2>
-  //       </div>
-  //       <div className="grid grid-cols-1 gap-5 text-white md:grid-cols-[33%_auto]">
-  //         <div className="rounded-xl bg-zinc-800 p-5">
-  //           <PDFViewer url="amongus" />
-  //         </div>
-  //         <Accordion
-  //           type="single"
-  //           collapsible
-  //           className="w-full rounded-xl bg-zinc-800 p-5 pb-8"
-  //         >
-  //           <AccordionItem value="item-1">
-  //             <AccordionTrigger>Grammar: 5/5</AccordionTrigger>
-  //             <AccordionContent>
-  //               Yes. It adheres to the WAI-ARIA design pattern.
-  //             </AccordionContent>
-  //           </AccordionItem>
-  //           <AccordionItem value="item-2">
-  //             <AccordionTrigger>Action Verbs: 5/5</AccordionTrigger>
-  //             <AccordionContent>
-  //               Yes. It comes with default styles that matches the other
-  //               components&apos; aesthetic.
-  //             </AccordionContent>
-  //           </AccordionItem>
-  //           <AccordionItem value="item-3">
-  //             <AccordionTrigger>Content: 5/5</AccordionTrigger>
-  //             <AccordionContent>
-  //               Yes. It&apos;s animated by default, but you can disable it if
-  //               you prefer.
-  //             </AccordionContent>
-  //           </AccordionItem>
-  //           <AccordionItem value="item-4">
-  //             <AccordionTrigger>
-  //               (Optional) Keyword Matching: 5/5
-  //             </AccordionTrigger>
-  //             <AccordionContent>
-  //               Yes. It&apos;s animated by default, but you can disable it if
-  //               you prefer.
-  //             </AccordionContent>
-  //           </AccordionItem>
-  //         </Accordion>
-  //       </div>
-  //       <Button
-  //         className="mt-5 w-full bg-sky-500"
-  //         onClick={() => setSubmittedResume(!submittedResume)}
-  //       >
-  //         rate another resume
-  //       </Button>
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <>
-  //       <div className="page-body grid grid-cols-1 gap-5 md:grid-cols-2">
-  //         <div className="input-box w-full rounded-xl bg-zinc-800 p-5">
-  //           <h2 className="font-bold text-neutral-200">job posting:</h2>
-  //           <Textarea
-  //             className="h-[55dvh] resize-none rounded-xl border border-neutral-200 bg-zinc-800 text-neutral-200"
-  //             placeholder="<Paste job description here. If you’re interested in general feedback, leave this empty.>"
-  //           />
-  //         </div>
-  //         <div className="resume-sec w-full rounded-xl bg-zinc-800 p-5">
-  //           <h2 className="font-bold text-neutral-200">your resume:</h2>
-  //           <div className="grid w-full">
-  //             <Select>
-  //               <SelectTrigger className="mb-5 w-full text-zinc-800">
-  //                 <SelectValue placeholder="Select resume or upload" />
-  //               </SelectTrigger>
-  //               <SelectContent>
-  //                 <SelectItem value="upload">Upload</SelectItem>
-  //                 <SelectItem value="Resume 1">Resume 1</SelectItem>
-  //                 <SelectItem value="Resume 1 (1)">Resume 1 (1)</SelectItem>
-  //               </SelectContent>
-  //             </Select>
-  //             <AddFileCard />
-  //           </div>
-  //           <Button
-  //             className="mt-5 w-full bg-sky-500"
-  //             onClick={() => setSubmittedResume(!submittedResume)}
-  //           >
-  //             RATE IT BABY!!!
-  //           </Button>
-  //           <div className="submit button"></div>
-  //         </div>
-  //       </div>
-  //     </>
-  //   );
-  // }
 };
 
 export default Rating;
