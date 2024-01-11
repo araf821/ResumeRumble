e5d10e6052b014fe52d0112a5d2ce6f06f3ca00d
araf821 <araf.ahmed200@gmail.com>
2024-01-11 02:31:58 -0500

    fix: error handling for database sleeping errors

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,18 @@
-'use client'
+"use client";
 
 import EmptyState from "@/components/EmptyState";
 
-const Error = ({}) => {
+export const dynamic = "force-dynamic";
+const Error = ({ error }: { error: Error & { digest?: string } }) => {
+  if (error.message.includes("prisma")) {
+    return (
+      <EmptyState
+        title="The database is sleeping."
+        description="Database has been put to sleep due to it being inactive for an extended period."
+      />
+    );
+  }
+
   return (
     <EmptyState
       title="Something went wrong."
